fd2f5094b469745807d1ed9a0e34009a817e4252
邢万祥 <xingwanxiang@llzg.cn>
2016-11-08 17:03:00 +0800

    FragmentSample from http://blog.csdn.net/xyz_lmn/article/details/6930753

---
+++ app/src/main/java/com/example/xingwx/fragmentsample/Shakespeare.java
@@ -0,0 +1,35 @@
+package com.example.xingwx.fragmentsample;
+
+public class Shakespeare {
+
+    /**
+     * Our data, part 1.
+     */
+    public static final String[] TITLES = 
+    {
+            "tittle1",   
+            "tittle2",
+            "tittle3",       
+            "tittle4",
+            "tittle5",
+            "tittle6",  
+            "tittle7",
+            "tittle8"
+    };
+    
+    /**
+     * Our data, part 2.
+     */
+    public static final String[] DIALOGUE = 
+    {
+            "this is tittle1.",
+            "this is tittle2.",
+            "this is tittle3.",
+            "this is tittle4.",
+            "this is tittle5.",
+            "this is tittle6.",
+            "this is tittle7.",
+            "this is tittle8."
+    };
+
+}
